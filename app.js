45f4fc3645fdf205f8699cc1e15a689dada21bf4
Etril <a.croissantguyamier@gmail.com>
2024-11-16 14:12:59 +0100

    Ajout du serveur, de l'app Express, et de la structure basique

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bookRoutes= require("./routes/books.js");
+
+const app=express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+mongoose.connect("mongodb+srv://acroissantguyamier:Aec5XYV5yCFoJLMY@ocprojet6.80lov.mongodb.net/?retryWrites=true&w=majority&appName=OCProjet6",   { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+app.use(express.json());
+
+
+
+
+app.use("/api/books", bookRoutes);
+
+
+module.exports=app;
---
+++ controllers/books.js
@@ -0,0 +1,15 @@
+const Book= require("../models/Book");
+
+
+
+exports.getBook= (req, res, next) => {
+    Book.findOne({ _id: req.params.id})
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(400).json({error}));
+};
+
+exports.getList= (req, res, next) => {
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({error}));
+  };
