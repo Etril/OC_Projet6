f9052dc68f78e12c6348af7eee7e41c8ca5ea0f9
Etril <a.croissantguyamier@gmail.com>
2024-11-16 23:07:36 +0100

    Ajout des routes, models et controllers pour le signup et login des utilisateurs

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bookRoutes= require("./routes/books.js");
+const userRoutes= require("./routes/users.js")
+const cors= require('cors');
 
 const app=express();
+app.use(cors());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
 
-mongoose.connect("mongodb+srv://acroissantguyamier:Aec5XYV5yCFoJLMY@ocprojet6.80lov.mongodb.net/?retryWrites=true&w=majority&appName=OCProjet6",   { useNewUrlParser: true,
-    useUnifiedTopology: true })
+
+mongoose.connect("mongodb+srv://acroissantguyamier:Aec5XYV5yCFoJLMY@ocprojet6.80lov.mongodb.net/?retryWrites=true&w=majority&appName=OCProjet6")
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 app.use(express.json());
@@ -21,6 +18,6 @@ app.use(express.json());
 
 
 app.use("/api/books", bookRoutes);
-
+app.use("/api/auth", userRoutes);
 
 module.exports=app;
