3f41b8d905a9de9d094e17cfe29bbcd67f7de94a
Etril <a.croissantguyamier@gmail.com>
2024-11-29 15:01:37 +0100

    Ajout des variables d'environnement

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bookRoutes= require("./routes/books.js");
-const userRoutes= require("./routes/users.js")
+const userRoutes= require("./routes/users.js");
 const cors= require('cors');
 const path = require ("path");
 const helmet= require("helmet");
@@ -13,10 +13,11 @@ app.use(helmet(
   {crossOriginResourcePolicy: false}));
 
 
-
 mongoose.connect("mongodb+srv://acroissantguyamier:Aec5XYV5yCFoJLMY@ocprojet6.80lov.mongodb.net/?retryWrites=true&w=majority&appName=OCProjet6")
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
 app.use(express.json());
 
 
